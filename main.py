d968309217862c1e1676ccc1d87d8361625050c2
Prince Khunt <princekhuntdatastructure@gmail.com>
2026-03-22 17:47:10 +0530

    v5

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import os
 from services.auth.login_wall import render_login_wall
 from services.state.session_defaults import initial_session_defaults
 from services.config.workout_config import EXERCISE_OPTIONS
+from services.ui.style_loader import load_css, inject_local_font
 
-
+  
 def main():
     st.set_page_config(
         page_icon="🏋️‍♀️",
@@ -12,9 +14,12 @@ def main():
         layout="centered"
     )
 
+    load_css(os.path.join(os.getcwd(), "static", "style.css"))
+    inject_local_font(os.path.join(os.getcwd(), "static", "AdobeClean.otf"), "AdobeClean")
+
     if not render_login_wall():
         return 
-    
+
     initial_session_defaults()
 
     workout_started = st.session_state.get("workout_started", False)
